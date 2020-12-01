2cb46e79fd60782fd2b2e840930c78730ca77374
武晓蒙 <18730013953@163.com>
2020-12-01 20:47:27 +0800

    feat: 添加测试案例

---
+++ src/views/demo/demo1.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+    {{ msg }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Demo1',
+  data() {
+    return {
+      msg: '智能设备检测研究所',
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
