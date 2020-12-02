fe6561b4de54e345309a20a18cc6729ca8e68c8d
武晓蒙 <18730013953@163.com>
2020-12-02 16:58:47 +0800

    test: 测试

--- src/views/web/auth/Login.vue
+++ src/views/web/auth/Login.vue
@@ -1,3 +1,21 @@
 <template>
-  <div>登录测试提交</div>
+  <div class="container">
+    {{ msg }}
+  </div>
 </template>
+
+<script>
+export default {
+  name: 'Login',
+  data() {
+    return {
+      msg: '智能设备检测研究1111',
+    };
+  },
+  methods: {
+    test() {},
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
